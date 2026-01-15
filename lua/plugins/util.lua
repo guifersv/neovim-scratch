0322ac8710db568d119d75eb79c9efecd210cef9
Guilherme Fernandes <gui.fer.sv@gmail.com>
2026-01-15 15:01:33 -0300

    Some utils

--- lua/plugins/util.lua
+++ lua/plugins/util.lua
@@ -37,4 +37,14 @@ return {
 		version = "*",
 		config = true,
 	},
+	{
+		"MeanderingProgrammer/render-markdown.nvim",
+		dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-mini/mini.nvim" }, -- if you use the mini.nvim suite
+		-- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-mini/mini.icons' },        -- if you use standalone mini plugins
+		-- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+		---@module 'render-markdown'
+		---@type render.md.UserConfig
+		opts = {},
+	},
+	{ "mluders/comfy-line-numbers.nvim", opts = {} },
 }
