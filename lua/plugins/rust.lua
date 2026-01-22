12722988be32a5c8d8378f02c93d1213f165f907
Guilherme Fernandes <gui.fer.sv@gmail.com>
2026-01-22 15:51:43 -0300

    Fix crate.nvim

--- lua/plugins/rust.lua
+++ lua/plugins/rust.lua
@@ -1,9 +1,19 @@
 return {
-	{
-		"saecki/crates.nvim",
-		tag = "stable",
-		opts = {},
-	},
+  {
+    "Saecki/crates.nvim",
+    event = { "BufRead Cargo.toml" },
+    opts = {
+      completion = {
+        crates = { enabled = true },
+      },
+      lsp = {
+        enabled = true,
+        actions = true,
+        completion = true,
+        hover = true,
+      },
+    },
+  },
 	{
 		"mrcjkb/rustaceanvim",
 		version = "^6",
